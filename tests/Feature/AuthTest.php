bf54eba122d101283932b3e02d0b48c2af6902d7
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2025-09-19 23:57:10 -0300

    Criar o primeiro teste funcional

---
+++ tests/Feature/AuthTest.php
@@ -0,0 +1,26 @@
+<?php
+
+
+it('display the login page when not logget in', function () {
+
+    // testando se houve redirecionamento pra rota "/login".
+    $result = $this->get('/')->assertRedirect("/login");
+
+    // testando se o status é 302
+    expect($result->status())->toBe(302);
+
+    // tentado se a rota de login está certa e retorna status 200
+    expect($this->get('/login')->status())->toBe(200);
+
+    // verifica se existe o texto de "Esqueceu a sua senha?" na tela
+    expect($this->get('/login')->content())->toContain("Esqueceu a sua senha?");
+});
+
+
+it("Forgot password", function () {
+    $result = $this->get('/forgot-password');
+
+    expect($result->status())->toBe(200);
+
+    expect($result->content())->toContain("Já sei a minha senha?");
+});
